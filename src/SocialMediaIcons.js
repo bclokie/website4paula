7787056a003b32260810d09a62d2c3ba82ea7008
Braden <braden.clokie@gmail.com>
2023-10-26 13:29:11 -0400

    Added React Router

--- src/SocialMediaIcons.js
+++ src/SocialMediaIcons.js
@@ -10,15 +10,6 @@ const SocialMediaIcons = () => {
       <a href="https://twitter.com/paula_voorheis/" target="_blank" rel="noopener noreferrer">
         <i className="fab fa-twitter"></i>
       </a>
-      <a href="https://www.instagram.com/paulavoorheis/" target="_blank" rel="noopener noreferrer">
-        <i className="fab fa-instagram"></i>
-      </a>
-      <a href="https://www.facebook.com/paula.voorheis/" target="_blank" rel="noopener noreferrer">
-        <i className="fab fa-facebook"></i>
-      </a>
-      <a href="https://www.pinterest.com/" target="_blank" rel="noopener noreferrer">
-        <i className="fab fa-pinterest"></i>
-      </a>
     </div>
   );
 };
