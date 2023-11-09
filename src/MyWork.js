0e21ea7c9289664e33e640ad602376c20e507a93
Braden <braden.clokie@gmail.com>
2023-11-09 13:18:43 -0500

    Updated My Work styling

--- src/MyWork.js
+++ src/MyWork.js
@@ -1,24 +1,28 @@
 import React from 'react';
-import './MyWork.css'; // Import the MyWork.css file for styling
+import './MyWork.css';
 
 const MyWork = () => {
   return (
     <div className="my-work-container">
-      <h2 className="work-title">My Work</h2>
-      <div className="work-item">
-        <img src="/work-image1.png" alt="Work 1" />
-        <h3>Tools and Models</h3>
-        <p>See the tools and models I use for my research</p>
+      <div className="work-title">
+        <h2>My Work</h2>
       </div>
-      <div className="work-item">
-        <img src="/work-image2.png" alt="Work 2" />
-        <h3>Publications</h3>
-        <p>Look through my published articles</p>
-      </div>
-      <div className="work-item">
-        <img src="/work-image3.png" alt="Work 3" />
-        <h3>Talks and Workshops</h3>
-        <p>Here are some other resources to look through</p>
+      <div className="work-items">
+        <div className="work-item">
+          <img src="/work-image1.png" alt="Work 1" />
+          <h3>Tools and Models</h3>
+          <p>See the tools and models I use for my research</p>
+        </div>
+        <div className="work-item">
+          <img src="/work-image2.png" alt="Work 2" />
+          <h3>Publications</h3>
+          <p>Look through my published articles</p>
+        </div>
+        <div className="work-item">
+          <img src="/work-image3.png" alt="Work 3" />
+          <h3>Talks and Workshops</h3>
+          <p>Here are some other resources to look through</p>
+        </div>
       </div>
     </div>
   );
