59a177b7c4bf962a7ad317e86be3fe7c2a232cef
Braden <braden.clokie@gmail.com>
2023-11-10 14:06:42 -0500

    Updated Contacts page

--- src/Contact.js
+++ src/Contact.js
@@ -1,21 +1,31 @@
 import React from 'react';
-import './Contact.css'; // Import the Contact.css file for styling
-import SocialMediaIcons from './SocialMediaIcons'; // Import the SocialMediaIcons component
+import { FaEnvelope, FaLinkedin, FaTwitter } from 'react-icons/fa';
+import './Contact.css';
 
 const Contact = () => {
   return (
     <div className="contact-container">
-      <h2 className="contact-title">Please Reach Out!</h2>
-      <div className="contact-info">
-        <div className="email-section">
-          <h3>Email</h3>
-          <p>paulavoorheis@gmail.com</p>
-        </div>
-        <div className="social-media-section">
-          <h3>Connect with Me</h3>
-          <SocialMediaIcons />
-        </div>
-      </div>
+      <h2 className="contact-title">Feel free to contact me!</h2>
+
+      <a href="mailto:paulavoorheis@gmail.com" className="contact-section">
+        <FaEnvelope className="icon" />
+        <p>paulavoorheis@gmail.com</p>
+      </a>
+
+      <a href="https://www.linkedin.com/in/paula-voorheis-phd-14415b11a/" target="_blank" rel="noopener noreferrer" className="contact-section">
+        <FaLinkedin className="icon" />
+        <p>LinkedIn</p>
+      </a>
+
+      <a href="https://twitter.com/paula_voorheis/" target="_blank" rel="noopener noreferrer" className="contact-section">
+        <FaTwitter className="icon" />
+        <p>Twitter</p>
+      </a>
+
+      <a href="https://scholar.google.ca/citations?hl=en&user=X5tw-kEAAAAJ&view_op=list_works&gmla=AJ1KiT1Wnnb5ARgiGLnOWFW1hoyDwPLSMEx-70qeAXjjVL0l4KyrzsxuqFp1r-MxtbkASbS-a6tcQ5DBCHJEz4Jw" target="_blank" rel="noopener noreferrer" className="contact-section">
+        <i className="ai ai-google-scholar icon"></i>
+        <p>Google Scholar</p>
+      </a>
     </div>
   );
 };
