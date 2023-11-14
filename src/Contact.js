11883a49d571d57af34fa2e33806c3ce1a5587c2
Braden <braden.clokie@gmail.com>
2023-11-14 12:04:33 -0500

    Forgot to push updates from last night

--- src/Contact.js
+++ src/Contact.js
@@ -5,6 +5,11 @@ import './Contact.css';
 
 const Contact = () => {
   return (
+    <div className="contact-page">
+    <div className="contact-header">
+      <h1>Contact</h1>
+      <div className="header-line"></div>
+    </div>
     <div className="contact-container">
       <div className="contact-links">
         <a href="mailto:paulavoorheis@gmail.com" className="contact-section">
@@ -37,26 +42,21 @@ const Contact = () => {
       </div>
 
       <div className="contact-form">
-        <h2>Contact Form</h2>
+        <h2>Send Me an E-Mail!</h2>
         <form>
           <div className="name-email-container">
-            <label htmlFor="name">Name:</label>
-            <input type="text" id="name" name="name" required />
-
-            <label htmlFor="email">Email:</label>
-            <input type="email" id="email" name="email" required />
+            <input type="text" id="name" name="name" placeholder="Your Name" required />
+            <input type="email" id="email" name="email" placeholder="Your Email" required />
           </div>
 
-          <label htmlFor="subject">Subject:</label>
-          <input type="text" id="subject" name="subject" required />
-
-          <label htmlFor="message">Message:</label>
-          <textarea id="message" name="message" rows="4" required></textarea>
+          <input type="text" id="subject" name="subject" placeholder="Subject" required />
+          <textarea id="message" name="message" rows="4" placeholder="Message" required></textarea>
 
           <button type="submit">Send Message</button>
         </form>
       </div>
     </div>
+  </div>
   );
 };
 
