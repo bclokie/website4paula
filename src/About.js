bde77016f7d380ed981179a56fe72861aa45db8b
Braden <braden.clokie@gmail.com>
2023-11-14 15:17:01 -0500

    Matched About page styling with other pages

--- src/About.js
+++ src/About.js
@@ -1,20 +1,26 @@
+// About.js
 import React from 'react';
-import './About.css'; // Import the About.css file for styling
+import './About.css';
 
 const About = () => {
   return (
     <div className="about-container">
-      <div className="about-image">
-        <img src="/about-image.png" alt="About Paula" />
-      </div>
-      <div className="about-text">
+      <div className="about-header">
         <h2>About Paula</h2>
-        <p>
-          I'm a researcher who uses best practices from behavioural science, implementation science,
-          and design science to improve the development and evaluation of health-related innovations.
-          I use mixed methods research approaches and work closely with stakeholders (e.g., patients,
-          caregivers, and providers) to ensure innovative solutions meet users’ needs.
-        </p>
+        <div className="header-line"></div>
+      </div>
+      <div className="about-content">
+        <div className="about-image">
+          <img src="/about-image.png" alt="About Paula" />
+        </div>
+        <div className="about-text">
+          <p>
+            I'm a researcher who uses best practices from behavioural science, implementation science,
+            and design science to improve the development and evaluation of health-related innovations.
+            I use mixed methods research approaches and work closely with stakeholders (e.g., patients,
+            caregivers, and providers) to ensure innovative solutions meet users’ needs.
+          </p>
+        </div>
       </div>
     </div>
   );
