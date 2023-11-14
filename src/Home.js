440cdbbd64e1b69ca2fdf327788a2cac51802b85
Braden <braden.clokie@gmail.com>
2023-11-14 15:27:27 -0500

    Added button to home page

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,7 @@
+// Home.js
 import React from 'react';
-import './Home.css'; 
+import { Link } from 'react-router-dom';
+import './Home.css';
 
 const Home = () => {
   return (
@@ -7,9 +9,12 @@ const Home = () => {
       <div className="header">
         <h1>Paula Voorheis PhD</h1>
         <p>Driving Innovation through Behaviourally Informed Design Research</p>
+        <Link to="/about" className="about-button">
+          About Me
+        </Link>
       </div>
       <div className="home-image">
-        <img src="/home-paula.png" alt="Profile" />      
+        <img src="/home-paula.png" alt="Profile" />
       </div>
     </div>
   );
