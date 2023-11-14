863680be9fce76ec65ed2a39e6ebc3f13387a035
Braden <braden.clokie@gmail.com>
2023-11-14 14:06:03 -0500

    Fixed the header line

--- src/RecentUpdates.js
+++ src/RecentUpdates.js
@@ -3,7 +3,7 @@ import './RecentUpdates.css'; // Import the RecentUpdates.css file for styling
 
 const RecentUpdates = () => {
   return (
-    <div className="recent-updates-container">
+    <div className="page-container">
       {/* Header Details */}
       <div className="contact-header">
         <h1>Recent Updates</h1>
@@ -11,22 +11,24 @@ const RecentUpdates = () => {
       </div>
 
       {/* Recent Updates Content */}
-      <div className="recent-updates-info">
-        <img src="/recent-updates.png" alt="Recent Updates" />
-      </div>
+      <div className="recent-updates-container">
+        <div className="recent-updates-info">
+          <img src="/recent-updates.png" alt="Recent Updates" />
+        </div>
 
-      <div className="update-container">
-        <div className="update">
-          <h3>Update 1</h3>
-          <p>Here is the content for the first update. Waiting for Paula to update the update</p>
-        </div>
-        <div className="update">
-          <h3>Update 2</h3>
-          <p>Here is the content for the second update. Waiting for Paula to update the update</p>
-        </div>
-        <div className="update">
-          <h3>Update 3</h3>
-          <p>Here is the content for the third update. Waiting for Paula to update the update</p>
+        <div className="update-container">
+          <div className="update">
+            <h3>Update 1</h3>
+            <p>Here is the content for the first update. Waiting for Paula to update the update</p>
+          </div>
+          <div className="update">
+            <h3>Update 2</h3>
+            <p>Here is the content for the second update. Waiting for Paula to update the update</p>
+          </div>
+          <div className="update">
+            <h3>Update 3</h3>
+            <p>Here is the content for the third update. Waiting for Paula to update the update</p>
+          </div>
         </div>
       </div>
     </div>
