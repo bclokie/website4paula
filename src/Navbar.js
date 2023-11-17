93bf63656414f6efb00498e025d3474d36c5da54
Braden <braden.clokie@gmail.com>
2023-11-17 13:53:29 -0500

    Navbar now closes when a link is closed in mobile view

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { NavLink } from 'react-router-dom'; // If you're using React Router
+import { NavLink } from 'react-router-dom';
 import './Navbar.css';
 import SocialMediaIcons from './pages/SocialMediaIcons';
 
@@ -11,25 +11,30 @@ const Navbar = () => {
     setIsOpen(!isOpen);
   };
 
+  const handleLinkClick = () => {
+    // Close the menu when a link is clicked
+    setIsOpen(false);
+  };
+
   return (
     <nav className="navbar">
-      <NavLink to="/" className="logo" onClick={() => setActiveLink('/')}>
+      <NavLink to="/" className="logo" onClick={() => { setActiveLink('/'); handleLinkClick(); }}>
         Paula Voorheis
       </NavLink>
       <div className="menu-icon" onClick={toggleMenu}>
         <i className={isOpen ? 'fas fa-times' : 'fas fa-bars'}></i>
       </div>
       <div className={`nav-links ${isOpen ? 'active' : ''}`}>
-        <NavLink to="/about" onClick={() => setActiveLink('/about')} activeClassName="active-link">
+        <NavLink to="/about" onClick={() => { setActiveLink('/about'); handleLinkClick(); }} activeClassName="active-link">
           About
         </NavLink>
-        <NavLink to="/my-work" onClick={() => setActiveLink('/my-work')} activeClassName="active-link">
+        <NavLink to="/my-work" onClick={() => { setActiveLink('/my-work'); handleLinkClick(); }} activeClassName="active-link">
           My Work
         </NavLink>
-        <NavLink to="/recent-updates" onClick={() => setActiveLink('/recent-updates')} activeClassName="active-link">
+        <NavLink to="/recent-updates" onClick={() => { setActiveLink('/recent-updates'); handleLinkClick(); }} activeClassName="active-link">
           Updates
         </NavLink>
-        <NavLink to="/contact" onClick={() => setActiveLink('/contact')} activeClassName="active-link">
+        <NavLink to="/contact" onClick={() => { setActiveLink('/contact'); handleLinkClick(); }} activeClassName="active-link">
           Contact
         </NavLink>
       </div>
