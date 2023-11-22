52a6fa38456dd5a699c5e802de55cfa94e8215c8
Braden <braden.clokie@gmail.com>
2023-11-22 15:38:54 -0500

    Added placeholder image

--- src/pages/ToolsandModels.js
+++ src/pages/ToolsandModels.js
@@ -13,17 +13,17 @@ const ToolsandModels = () => {
       </div>
       <div className="work-items">
         <div className="work-item">
-          <img src="/tool-image1.png" alt="Tool 1" />
+          <img src="https://picsum.photos/300/200" alt="Tool 1" />
           <h3>Tool 1 Title</h3>
           <p>Tool 1 description goes here.</p>
         </div>
         <div className="work-item">
-          <img src="/tool-image2.png" alt="Tool 2" />
+          <img src="https://picsum.photos/300/200" alt="Tool 2" />
           <h3>Tool 2 Title</h3>
           <p>Tool 2 description goes here.</p>
         </div>
         <div className="work-item">
-          <img src="/tool-image3.png" alt="Tool 3" />
+          <img src="https://picsum.photos/300/200" alt="Tool 3" />
           <h3>Tool 3 Title</h3>
           <p>Tool 3 description goes here.</p>
         </div>
