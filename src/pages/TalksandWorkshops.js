dff4d3418c87d884c89646d03859b69c0d2512d4
Braden <braden.clokie@gmail.com>
2023-11-29 11:39:11 -0500

    Replaced placeholder code for Talks and Workshops, introduced new template

--- src/pages/TalksandWorkshops.js
+++ src/pages/TalksandWorkshops.js
@@ -1,32 +1,66 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './TalksAndWorkshops.css';
+// TalksandWorkshops.js
 
-const TalksAndWorkshops = () => {
+import React from 'react';
+import './TalksandWorkshops.css';
+
+const TalksandWorkshops = () => {
   return (
-    <div className="talks-and-workshops-container">
-      <div className="work-title">
-        <h2>Talks and Workshops</h2>
+    <div className="talks-workshops-container">
+      {/* Section 1: Academic Speaking and Presenting */}
+      <div className="talks-workshops-section">
+        <h2>Academic Speaking and Presenting</h2>
+        <p>
+          I have had the opportunity to present at many different academic conferences in my field. Below is a short list of key conferences I have presented at.
+        </p>
+        <div className="talks-workshops-images">
+          {/* Insert 5 images */}
+          {[...Array(5)].map((_, index) => (
+            <img key={index} src="https://via.placeholder.com/200" alt={`Conference ${index + 1}`} />
+          ))}
+        </div>
       </div>
-      <div className="work-items">
-        <Link to="/upcoming-talks" className="work-item">
-          <img src="/talk-image1.png" alt="Talk 1" />
-          <h3>Upcoming Talks</h3>
-          <p>Find information about my upcoming talks</p>
-        </Link>
-        <Link to="/past-talks" className="work-item">
-          <img src="/talk-image2.png" alt="Talk 2" />
-          <h3>Past Talks</h3>
-          <p>Explore details about my past presentations and talks</p>
-        </Link>
-        <Link to="/workshops" className="work-item">
-          <img src="/workshop-image1.png" alt="Workshop 1" />
-          <h3>Workshops</h3>
-          <p>Discover the workshops I conduct and have conducted</p>
-        </Link>
+
+      {/* Section 2: Co-Design Workshops */}
+      <div className="talks-workshops-section">
+        <h2>Co-Design Workshops</h2>
+        <p>
+          I have had the opportunity to run several design workshops for organizations in need of expertise on co-design and designing behavior change interventions. Below are some of the organizations I have worked with to run these workshops.
+        </p>
+        <div className="talks-workshops-images">
+          {/* Insert 3 images */}
+          {[...Array(3)].map((_, index) => (
+            <img key={index} src="https://via.placeholder.com/200" alt={`Workshop Organization ${index + 1}`} />
+          ))}
+        </div>
+      </div>
+
+      {/* Section 3: Company Presentations */}
+      <div className="talks-workshops-section">
+        <h2>Company Presentations</h2>
+        <p>
+          I am available to give presentations to companies and organizations interested in educating their staff on designing interventions using insights from behavioral science and design science. Below are some of the companies I have presented to.
+        </p>
+        <div className="talks-workshops-images">
+          {/* Insert 2 images */}
+          {[...Array(2)].map((_, index) => (
+            <img key={index} src="https://via.placeholder.com/200" alt={`Company ${index + 1}`} />
+          ))}
+        </div>
+      </div>
+
+      {/* Section 4: Teaching */}
+      <div className="talks-workshops-section">
+        <h2>Teaching</h2>
+        <p>
+          My teaching has involved guest lecturing for several courses at the Institute of Health Policy, Management and Evaluation at the University of Toronto.
+        </p>
+        <div className="talks-workshops-images">
+          {/* Insert 1 image */}
+          <img src="https://via.placeholder.com/200" alt="Teaching" />
+        </div>
       </div>
     </div>
   );
 };
 
-export default TalksAndWorkshops;
+export default TalksandWorkshops;
