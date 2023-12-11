d576577e77a9ef97c1e055e37c87535255d9ddfc
Braden <braden.clokie@gmail.com>
2023-12-11 14:12:10 -0500

    Changed methods to models, always mess this up 🤦🏻‍♂️

--- src/pages/ToolsandModels.js
+++ src/pages/ToolsandModels.js
@@ -11,32 +11,32 @@ const ToolsandModels = () => {
   };
   
   return (
-    <div className="tools-methods-container">
-      <div className="tools-methods-title-container">
-        <h2 className="tools-methods-title">Tools and Models</h2>
-        <div className="tools-methods-header-line"></div>
+    <div className="tools-models-container">
+      <div className="tools-models-title-container">
+        <h2 className="tools-models-title">Tools and Models</h2>
+        <div className="tools-models-header-line"></div>
       </div>
 
       {/* Section 1: Behavioral Design Thinking Approach */}
-      <div className="tools-methods-section-container">
-        <h3 className="tools-methods-section-title">Behavioral Design Thinking Approach</h3>
-        <p className="tools-methods-section-subtitle">
+      <div className="tools-models-section-container">
+        <h3 className="tools-models-section-title">Behavioral Design Thinking Approach</h3>
+        <p className="tools-models-section-subtitle">
           Behavioral Design Thinking (BDT) is a novel approach to designing behavior change interventions,
           integrating best practices from Behavioral Design (BD) and Design Thinking (DT).
           The tools below will help you navigate the BDT process.
         </p>
-        <div className="tools-methods-work-items">
-          <div className="tools-methods-work-item" onClick={() => handleImageClick("/TM1-1.png")}>
+        <div className="tools-models-work-items">
+          <div className="tools-models-work-item" onClick={() => handleImageClick("/TM1-1.png")}>
             <h3>Playbook</h3>
             <img src="/TM1-1.png" alt="Playbook" />
             <p>Behavioral Design Thinking</p>
           </div>
-          <div className="tools-methods-work-item" onClick={() => handleImageClick("/TM1-2.png")}>
+          <div className="tools-models-work-item" onClick={() => handleImageClick("/TM1-2.png")}>
             <h3>Model</h3>
             <img src="/TM1-2.png" alt="Model" />
-            <p>Design Methods for “Effectively Engaging” Digital Health Interventions</p>
+            <p>Design models for “Effectively Engaging” Digital Health Interventions</p>
           </div>
-          <div className="tools-methods-work-item" onClick={() => handleImageClick("/TM1-3.png")}>
+          <div className="tools-models-work-item" onClick={() => handleImageClick("/TM1-3.png")}>
             <h3>Insights</h3>
             <img src="/TM1-3.png" alt="Insights" />
             <p>Behavioral Design Thinking Considerations and Challenges</p>
@@ -45,25 +45,25 @@ const ToolsandModels = () => {
       </div>
 
       {/* Section 2: Behavioral Design Tools */}
-      <div className="tools-methods-section-container">
-        <h3 className="tools-methods-section-title">Behavioral Design Tools</h3>
-        <p className="tools-methods-section-subtitle">
+      <div className="tools-models-section-container">
+        <h3 className="tools-models-section-title">Behavioral Design Tools</h3>
+        <p className="tools-models-section-subtitle">
           Behavioral Design (BD) theories, models, and frameworks are helpful to design better behaviour
           change interventions; however, it can be hard to select which ones to choose. The tools below
           will help you select the best theories, models, and frameworks for your work.
         </p>
-        <div className="tools-methods-work-items">
-          <div className="tools-methods-work-item" onClick={() => handleImageClick("/TM2-1.png")}>
+        <div className="tools-models-work-items">
+          <div className="tools-models-work-item" onClick={() => handleImageClick("/TM2-1.png")}>
             <h3>Toolkit</h3>
             <img src="/TM2-1.png" alt="Toolkit" />
             <p>Types of Behavioral Design Tools</p>
           </div>
-          <div className="tools-methods-work-item" onClick={() => handleImageClick("/TM2-2.png")}>
+          <div className="tools-models-work-item" onClick={() => handleImageClick("/TM2-2.png")}>
             <h3>Guidance</h3>
             <img src="/TM2-2.png" alt="Guidance" />
             <p>How to Select Behavioral Design Tools</p>
           </div>
-          <div className="tools-methods-work-item" onClick={() => handleImageClick("/TM2-3.png")}>
+          <div className="tools-models-work-item" onClick={() => handleImageClick("/TM2-3.png")}>
             <h3>Recommendations</h3>
             <img src="/TM2-3.png" alt="Recommendations" />
             <p>How to Improve Behavioral Design Tools</p>
@@ -72,20 +72,20 @@ const ToolsandModels = () => {
       </div>
 
       {/* Section 3: Co-Designing Behaviour Change Interventions */}
-      <div className="tools-methods-section-container">
-        <h3 className="tools-methods-section-title">Co-Designing Behaviour Change Interventions</h3>
-        <p className="tools-methods-section-subtitle">
+      <div className="tools-models-section-container">
+        <h3 className="tools-models-section-title">Co-Designing Behaviour Change Interventions</h3>
+        <p className="tools-models-section-subtitle">
           Co-designing with patients, caregivers, and families can help ensure the behaviour change
           interventions you design better meet people’s needs. The tools below offer guidance on how
           to maximize the value of co-design.
         </p>
-        <div className="tools-methods-work-items">
-          <div className="tools-methods-work-item" onClick={() => handleImageClick("/TM3-1.png")}>
+        <div className="tools-models-work-items">
+          <div className="tools-models-work-item" onClick={() => handleImageClick("/TM3-1.png")}>
             <h3>Playbook</h3>
             <img src="/TM3-1.png" alt="Playbook" />
             <p>Co-Design Process for Maximal Value</p>
           </div>
-          <div className="tools-methods-work-item" onClick={() => handleImageClick("/TM3-2.png")}>
+          <div className="tools-models-work-item" onClick={() => handleImageClick("/TM3-2.png")}>
             <h3>Insights</h3>
             <img src="/TM3-2.png" alt="Insights" />
             <p>How Co-Design Adds Value</p>
@@ -94,14 +94,14 @@ const ToolsandModels = () => {
       </div>
 
       {/* Section 4: Strategic Behavioural Analysis */}
-      <div className="tools-methods-section-container">
-        <h3 className="tools-methods-section-title">Strategic Behavioural Analysis</h3>
-        <p className="tools-methods-section-subtitle">
+      <div className="tools-models-section-container">
+        <h3 className="tools-models-section-title">Strategic Behavioural Analysis</h3>
+        <p className="tools-models-section-subtitle">
           Strategic Behavioural Analysis (SBA) offers a systematic way to review the literature using a
           behavioural science lens. The tools below offer guidance on how to conduct a SBA.
         </p>
-        <div className="tools-methods-work-items">
-          <div className="tools-methods-work-item" onClick={() => handleImageClick("/TM4-1.png")}>
+        <div className="tools-models-work-items">
+          <div className="tools-models-work-item" onClick={() => handleImageClick("/TM4-1.png")}>
             <h3>Playbook</h3>
             <img src="/TM4-1.png" alt="Playbook" />
             <p>Strategic Behavioural Analysis</p>
@@ -109,14 +109,14 @@ const ToolsandModels = () => {
         </div>
       </div>
 
-      <Link to="/contact" className="tools-methods-updates-button">
+      <Link to="/contact" className="tools-models-updates-button">
         Contact Me!
       </Link>
       
       {/* Overlay for the clicked image */}
       {clickedImage && (
-        <div className="tools-methods-overlay" onClick={() => setClickedImage(null)}>
-          <img src={clickedImage} alt="Expanded" className="tools-methods-expanded-image" />
+        <div className="tools-models-overlay" onClick={() => setClickedImage(null)}>
+          <img src={clickedImage} alt="Expanded" className="tools-models-expanded-image" />
         </div>
       )}
     </div>
