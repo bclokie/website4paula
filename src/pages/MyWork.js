e686f9d095603b9550929c9cf1b9a71102c0c9bc
Braden <braden.clokie@gmail.com>
2024-01-02 16:59:47 -0500

    Problem solved! 👨🏻‍🔧

--- src/pages/MyWork.js
+++ src/pages/MyWork.js
@@ -1,9 +1,16 @@
 // MyWork.js
 import React from 'react';
-import { Link } from 'react-router-dom'; 
+import { Link, useNavigate } from 'react-router-dom'; 
 import './MyWork.css';
 
+
 const MyWork = () => {
+  const navigate = useNavigate();
+
+  const navigateTo = (path) => {
+    navigate(path);
+  };
+
   return (
     <div className="my-work-container">
       <div className="my-work-title-container">
@@ -11,21 +18,21 @@ const MyWork = () => {
         <div className="my-work-header-line"></div>
       </div>
       <div className="my-work-items">
-        <Link to="/tools+models" className="work-item">
+        <div className="work-item" onClick={() => navigateTo('/tools+models')}>
           <img src="/work-image1.png" alt="Work 1" />
           <h3>Tools and Models</h3>
           <p>See the tools and models I use for my research</p>
-        </Link>
-        <Link to="/publications" className="work-item">
+        </div>
+        <div className="work-item" onClick={() => navigateTo('/publications')}>
           <img src="/work-image2.png" alt="Work 2" />
           <h3>Publications</h3>
           <p>Look through my published articles</p>
-        </Link>
-        <Link to="/talks+workshops" className="work-item">
+        </div>
+        <div className="work-item" onClick={() => navigateTo('/talks+workshops')}>
           <img src="/work-image3.png" alt="Work 3" />
           <h3>Talks and Workshops</h3>
           <p>Here are some other resources to look through</p>
-        </Link>
+        </div>
       </div>
       <Link to="/contact" className="my-updates-button">
         Contact Me!
