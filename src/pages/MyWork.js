e434eb50f1e5f3af2ad40a3ed321957b29ff39b0
Braden <braden.clokie@gmail.com>
2024-01-02 16:42:19 -0500

    Still can't solve this underline issue...

--- src/pages/MyWork.js
+++ src/pages/MyWork.js
@@ -11,17 +11,17 @@ const MyWork = () => {
         <div className="my-work-header-line"></div>
       </div>
       <div className="my-work-items">
-        <Link to="/tools+models" className="my-work-item">
+        <Link to="/tools+models" className="work-item">
           <img src="/work-image1.png" alt="Work 1" />
           <h3>Tools and Models</h3>
           <p>See the tools and models I use for my research</p>
         </Link>
-        <Link to="/publications" className="my-work-item">
+        <Link to="/publications" className="work-item">
           <img src="/work-image2.png" alt="Work 2" />
           <h3>Publications</h3>
           <p>Look through my published articles</p>
         </Link>
-        <Link to="/talks+workshops" className="my-work-item">
+        <Link to="/talks+workshops" className="work-item">
           <img src="/work-image3.png" alt="Work 3" />
           <h3>Talks and Workshops</h3>
           <p>Here are some other resources to look through</p>
