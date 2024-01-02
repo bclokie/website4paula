0631e223ee7a27dd17faea283ea1773c81e2d029
Braden <braden.clokie@gmail.com>
2024-01-02 11:53:54 -0500

    Fixed underline issue with social media links in Navbar

--- src/pages/SocialMediaIcons.js
+++ src/pages/SocialMediaIcons.js
@@ -1,16 +1,18 @@
+// SocialMediaIcons.js
+
 import React from 'react';
 import './SocialMediaIcons.css';
 
 const SocialMediaIcons = () => {
   return (
     <div className="social-icons">
-      <a href="https://www.linkedin.com/in/paula-voorheis-phd-14415b11a/" target="_blank" rel="noopener noreferrer">
+      <a className="social-link" href="https://www.linkedin.com/in/paula-voorheis-phd-14415b11a/" target="_blank" rel="noopener noreferrer">
         <i className="fab fa-linkedin"></i>
       </a>
-      <a href="https://twitter.com/paula_voorheis/" target="_blank" rel="noopener noreferrer">
+      <a className="social-link" href="https://twitter.com/paula_voorheis/" target="_blank" rel="noopener noreferrer">
         <i className="fab fa-twitter"></i>
       </a>
-      <a href="https://scholar.google.ca/citations?hl=en&user=X5tw-kEAAAAJ&view_op=list_works&gmla=AJ1KiT1Wnnb5ARgiGLnOWFW1hoyDwPLSMEx-70qeAXjjVL0l4KyrzsxuqFp1r-MxtbkASbS-a6tcQ5DBCHJEz4Jw" target="_blank" rel="noopener noreferrer">
+      <a className="social-link" href="https://scholar.google.ca/citations?hl=en&user=X5tw-kEAAAAJ&view_op=list_works&gmla=AJ1KiT1Wnnb5ARgiGLnOWFW1hoyDwPLSMEx-70qeAXjjVL0l4KyrzsxuqFp1r-MxtbkASbS-a6tcQ5DBCHJEz4Jw" target="_blank" rel="noopener noreferrer">
         <i className="ai ai-google-scholar"></i> 
       </a>
     </div>
