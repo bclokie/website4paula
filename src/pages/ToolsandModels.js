4aa7927874fcfd5a23c77b8b0c3547f14e644888
Braden <braden.clokie@gmail.com>
2024-01-04 12:34:06 -0500

    Improved Tools & Models mobile view

--- src/pages/ToolsandModels.js
+++ src/pages/ToolsandModels.js
@@ -19,7 +19,9 @@ const ToolsandModels = () => {
 
       {/* Section 1: Behavioral Design Thinking Approach */}
       <div className="tools-models-section-container">
+        <hr className="divider" />
         <h3 className="tools-models-section-title">Behavioral Design Thinking Approach</h3>
+        <hr className="divider" />
         <p className="tools-models-section-subtitle">
           Behavioral Design Thinking (BDT) is a novel approach to designing behavior change interventions,
           integrating best practices from Behavioral Design (BD) and Design Thinking (DT).
@@ -31,11 +33,13 @@ const ToolsandModels = () => {
             <img src="/TM1-1.png" alt="Playbook" />
             <p>Behavioral Design Thinking</p>
           </div>
+          <hr className="divider" />
           <div className="tools-models-work-item" onClick={() => handleImageClick("/TM1-2.png")}>
             <h3>Model</h3>
             <img src="/TM1-2.png" alt="Model" />
             <p>Design models for “Effectively Engaging” Digital Health Interventions</p>
           </div>
+          <hr className="divider" />
           <div className="tools-models-work-item" onClick={() => handleImageClick("/TM1-3.png")}>
             <h3>Insights</h3>
             <img src="/TM1-3.png" alt="Insights" />
@@ -46,7 +50,9 @@ const ToolsandModels = () => {
 
       {/* Section 2: Behavioral Design Tools */}
       <div className="tools-models-section-container">
+        <hr className="divider" />
         <h3 className="tools-models-section-title">Behavioral Design Tools</h3>
+        <hr className="divider" />
         <p className="tools-models-section-subtitle">
           Behavioral Design (BD) theories, models, and frameworks are helpful to design better behaviour
           change interventions; however, it can be hard to select which ones to choose. The tools below
@@ -58,11 +64,13 @@ const ToolsandModels = () => {
             <img src="/TM2-1.png" alt="Toolkit" />
             <p>Types of Behavioral Design Tools</p>
           </div>
+          <hr className="divider" />
           <div className="tools-models-work-item" onClick={() => handleImageClick("/TM2-2.png")}>
             <h3>Guidance</h3>
             <img src="/TM2-2.png" alt="Guidance" />
             <p>How to Select Behavioral Design Tools</p>
           </div>
+          <hr className="divider" />
           <div className="tools-models-work-item" onClick={() => handleImageClick("/TM2-3.png")}>
             <h3>Recommendations</h3>
             <img src="/TM2-3.png" alt="Recommendations" />
@@ -73,7 +81,9 @@ const ToolsandModels = () => {
 
       {/* Section 3: Co-Designing Behaviour Change Interventions */}
       <div className="tools-models-section-container">
+        <hr className="divider" />
         <h3 className="tools-models-section-title">Co-Designing Behaviour Change Interventions</h3>
+        <hr className="divider" />
         <p className="tools-models-section-subtitle">
           Co-designing with patients, caregivers, and families can help ensure the behaviour change
           interventions you design better meet people’s needs. The tools below offer guidance on how
@@ -85,6 +95,7 @@ const ToolsandModels = () => {
             <img src="/TM3-1.png" alt="Playbook" />
             <p>Co-Design Process for Maximal Value</p>
           </div>
+          <hr className="divider" />
           <div className="tools-models-work-item" onClick={() => handleImageClick("/TM3-2.png")}>
             <h3>Insights</h3>
             <img src="/TM3-2.png" alt="Insights" />
@@ -95,7 +106,9 @@ const ToolsandModels = () => {
 
       {/* Section 4: Strategic Behavioural Analysis */}
       <div className="tools-models-section-container">
+        <hr className="divider" />
         <h3 className="tools-models-section-title">Strategic Behavioural Analysis</h3>
+        <hr className="divider" />
         <p className="tools-models-section-subtitle">
           Strategic Behavioural Analysis (SBA) offers a systematic way to review the literature using a
           behavioural science lens. The tools below offer guidance on how to conduct a SBA.
